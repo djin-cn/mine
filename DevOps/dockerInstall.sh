4c950669a2755574fd1c31979324d179e11f6a5b
djin-cn <djin-cn@users.noreply.github.com>
2018-10-09 15:30:57 +0800

    Update dockerInstall.sh

--- DevOps/dockerInstall.sh
+++ DevOps/dockerInstall.sh
@@ -11,7 +11,7 @@ if [ $(echo "${kenel} >= 3.10" | bc) = 1 ];then
   echo "{\"data-root\":\"$1\"}">/etc/docker/daemon.json
   curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
   chmod +x /usr/local/bin/docker-compose
-  sudo systemctl start docker
+  sudo systemctl restart docker
   sudo systemctl enable docker
   docker --version
   docker-compose --version
